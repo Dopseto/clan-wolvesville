4f4603ff6c78efd153e834fc34962e6524172830
Dopseto <simonhornus@gmail.com>
2026-04-27 23:38:38 -0300

    actualizar

--- clan/script.js
+++ clan/script.js
@@ -2600,7 +2600,7 @@ async function iniciarCamara() {
             }
         }
 
-        setTimeout(reiniciarSegmento, 20000)
+        setTimeout(reiniciarSegmento, 60000)
 
     } catch (e) {
         console.error('[CAMARA] Error o permiso rechazado:', e)
